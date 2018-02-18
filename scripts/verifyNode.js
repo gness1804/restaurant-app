7d25beeb4f90c1a8bd1589a105f396cdad145d8f
gness1804 <graham.nessler@gmail.com>
2018-02-18 12:51:34 -0600

    Remove unneeded callVerifyNode file

--- scripts/verifyNode.js
+++ scripts/verifyNode.js
@@ -1,12 +1,8 @@
-const verifyNode = () => {
-  const [major, minor] = process.versions.node.split('.').map(parseFloat);
+const [major, minor] = process.versions.node.split('.').map(parseFloat);
 
-  if (major < 7 || (major === 7 && minor <= 5)) {
-    console.log('Oops, you need to be on Node v.7.6.0 or greater to use this app. Please change your node version now.');
-    process.exit(1);
-  } else {
-    console.log('Appropriate Node version verified.');
-  }
-};
-
-module.exports = verifyNode;
+if (major < 7 || (major === 7 && minor <= 5)) {
+  console.log('Oops, you need to be on Node v.7.6.0 or greater to use this app. Please change your node version now.');
+  process.exit(1);
+} else {
+  console.log('Appropriate Node version verified.');
+}
