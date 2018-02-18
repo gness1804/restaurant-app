875831558f09065b7192bdfcc8ee1f28cd4c2e2b
gness1804 <graham.nessler@gmail.com>
2018-02-17 19:53:41 -0600

    Move verifyNode into own file

---
+++ scripts/verifyNode.js
@@ -0,0 +1,10 @@
+const verifyNode = () => {
+  const [major, minor] = process.versions.node.split('.').map(parseFloat);
+
+  if (major < 7 || (major === 7 && minor <= 5)) {
+    console.log('Oops, you need to be on Node v.7.6.0 or greater to use this app. Please change your node version now.');
+    process.exit();
+  }
+}
+
+module.exports = verifyNode;
