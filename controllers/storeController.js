db55f969599d31c3d6db7725aa39de785825b41b
gness1804 <graham.nessler@gmail.com>
2018-02-19 19:06:45 -0600

    Create first middleware

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,3 +1,11 @@
+exports.firstMiddleware = (req, res, next) => {
+  req.name = 'Graham';
+  next();
+};
+
 exports.homePage = (req, res) => {
-  res.render('index');
+  console.log(`The user's name is ${req.name}.`);
+  res.render('index', {
+    user: req.name,
+  });
 };
