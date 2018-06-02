7a269b5f92440ba31466dcd18e064ae4d4ea3e65
gness1804 <graham.nessler@gmail.com>
2018-06-02 17:36:12 -0500

    Finished with video 22

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -95,9 +95,10 @@ exports.getStoreBySlug = async (req, res, next) => {
 
 exports.getStoreByTag = async (req, res) => {
   const tag = req.params.tag;
+  const tagQuery = tag || { $exists: true };
   const tagsPromise = Store.getTagsList();
   const storesPromise = Store.find({
-    tags: tag,
+    tags: tagQuery,
   });
   const [tags, stores] = await Promise.all([tagsPromise, storesPromise]);
   res.render('tag', {
